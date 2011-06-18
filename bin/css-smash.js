240ca933dead6c7af66d5234c289afad07e0182a
Mark Bennett <mark@burmis.ca>
2011-06-18 15:31:47 -0600

    Output smashed files to an outfile.

--- bin/css-smash.js
+++ bin/css-smash.js
@@ -3,13 +3,16 @@
 var css = require("../lib/css"),
     nopt = require("nopt"),
     fs = require("fs"),
+    path = require("path"),
     knownOpts = {
-      "usage" : Boolean
+      "usage" : Boolean,
+      "outfile" : path
     },
     shortHands = {
-      "u" : ["--usage"]
+      "u" : ["--usage"],
+      "o" : ["--outfile"]
     },
-    usage = "Usage: css-smash [options] <cssfile1> <cssfile2> <cssfile3> ...\n\nThis compresses each of the specified css files and outputs the compressed results. By default css-smash outputs to stdout.\n\nWhere the options are:\n\n\t-o, --output\n\t\tPath to output compressed CSS to.\n\n\t-u, --usage\n\t\tDisplays this usage message.\n\nExamples:\n\n\tcss-smash my.css\n\tcss-smash -o smashed.css file1.css file2.css",
+    usage = "Usage: css-smash [options] <cssfile1> <cssfile2> <cssfile3> ...\n\nThis compresses each of the specified css files and outputs the compressed results. By default css-smash outputs to stdout.\n\nWhere the options are:\n\n\t-o, --outfile\n\t\tFile to output compressed CSS to.\n\n\t-u, --usage\n\t\tDisplays this usage message.\n\nExamples:\n\n\tcss-smash my.css\n\tcss-smash -o smashed.css file1.css file2.css",
     parsed = nopt(knownOpts, shortHands, process.argv);
 
 function die(why) {
@@ -23,6 +26,8 @@ if (parsed.usage) {
   process.exit(0);
 }
 
+console.log(parsed);
+
 if (!parsed.argv.remain.length) {
   die("No files specified!");
 }
@@ -33,7 +38,17 @@ function smashFile(file) {
       throw err;
     }
     data = data.toString("utf8");
-    console.log(css.smash(data));
+    smashed = css.smash(data);
+    if (parsed.outfile) {
+      fs.open(parsed.outfile, "w", function(err, fd) {
+        if (err) {
+          throw err;
+        }
+        fs.writeSync(fd, smashed);
+      });
+    } else {
+      console.log(smashed);
+    }
   });
 }
 
