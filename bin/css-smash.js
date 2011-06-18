7670d53dc00bbc37acc0a6ac04f522d843baf8ef
Mark Bennett <mark@burmis.ca>
2011-06-18 15:45:38 -0600

    CLI now smashes and outputs multiple files!

--- bin/css-smash.js
+++ bin/css-smash.js
@@ -13,6 +13,7 @@ var css = require("../lib/css"),
       "o" : ["--outfile"]
     },
     usage = "Usage: css-smash [options] <cssfile1> <cssfile2> <cssfile3> ...\n\nThis compresses each of the specified css files and outputs the compressed results. By default css-smash outputs to stdout.\n\nWhere the options are:\n\n\t-o, --outfile\n\t\tFile to output compressed CSS to.\n\n\t-u, --usage\n\t\tDisplays this usage message.\n\nExamples:\n\n\tcss-smash my.css\n\tcss-smash -o smashed.css file1.css file2.css",
+    smashed = [],
     parsed = nopt(knownOpts, shortHands, process.argv);
 
 function die(why) {
@@ -26,30 +27,29 @@ if (parsed.usage) {
   process.exit(0);
 }
 
-console.log(parsed);
-
 if (!parsed.argv.remain.length) {
   die("No files specified!");
 }
 
 function smashFile(file) {
-  fs.readFile(file, function(err, data) {
+  var data;
+
+  data = fs.readFileSync(file);
+  data = data.toString("utf8");
+  smashed.push(css.smash(data));
+};
+
+parsed.argv.remain.forEach(smashFile);
+
+smashed = smashed.join("\n");
+
+if (parsed.outfile) {
+  fs.open(parsed.outfile, "w", function(err, fd) {
     if (err) {
       throw err;
     }
-    data = data.toString("utf8");
-    smashed = css.smash(data);
-    if (parsed.outfile) {
-      fs.open(parsed.outfile, "w", function(err, fd) {
-        if (err) {
-          throw err;
-        }
-        fs.writeSync(fd, smashed);
-      });
-    } else {
-      console.log(smashed);
-    }
+    fs.writeSync(fd, smashed);
   });
+} else {
+  console.log(smashed);
 }
-
-parsed.argv.remain.forEach(smashFile);
